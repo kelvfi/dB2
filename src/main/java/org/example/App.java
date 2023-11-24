02410cac3dd6d7f548e0163e2c6675363079660b
kelvfi <126086807+kelvfi@users.noreply.github.com>
2023-11-24 18:58:55 +0100

    SELECT_Ausgabe

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,52 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.sql.*;
+
+public class App
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        showEverything();
+    }
+
+    /**
+     * Hier baut man einfach eine
+     * Verbindung zur Datenbank auf diese habe ich extra hier geschrieben
+     * damit ich sie nicht immer neu machen muss.
+     */
+    public static Connection connect() {
+        final String CONNECTION_URL = "jdbc:mysql://localhost:3306/db2"; // jdbc:mysql://localhost:3306/NAME | Das ist immer Standardmäßig und hinten kommt der Name
+        final String USER = "root";
+        final String PSWRD = "";
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(CONNECTION_URL, USER, PSWRD);
+            System.out.println("Verbindung zur Datenbank war ERFOLGREICH!");
+        } catch (SQLException e) {
+            System.out.println("FEHLER beim Verbinden zur Datenbank! "+e.getMessage());
+        }
+        return  conn;
+    }
+
+    public static void showEverything() {
+
+        try {
+
+            PreparedStatement preparedStatement = connect().prepareStatement("SELECT * FROM `lehrer`");
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int aS = rs.getInt("arbeitsstunden");
+
+                System.out.println("LEHRER | ID: "+id+" NAME: "+name+" ARBEITSSTUNDEN: "+aS);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Fehler beim Verbinden der Datenbank: "+e.getMessage());
+        }
     }
 }
